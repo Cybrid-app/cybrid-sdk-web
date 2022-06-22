c7d0d7670dcb4171b7ef03a612ae7ebbb5aaed17
dcocking <dustin.cocking@gmail.com>
2022-06-22 12:14:50 -0400

    cleanup

--- src/shared/services/quote/quote.service.ts
+++ src/shared/services/quote/quote.service.ts
@@ -41,7 +41,7 @@ export class QuoteService {
     side: PostQuoteBankModel.SideEnum,
     asset: AssetBankModel,
     counterAsset: AssetBankModel
-  ): any {
+  ): PostQuoteBankModel {
     const symbol = symbolBuild(asset.code, counterAsset.code);
     let postQuoteBankModel: PostQuoteBankModel = {
       customer_guid: this.customer_guid,
