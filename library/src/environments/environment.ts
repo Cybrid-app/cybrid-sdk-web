e4bf031b0378a1baf6fb18604732ebf8ea367fe6
dcocking <dustin.cocking@gmail.com>
2023-01-30 15:37:49 -0500

    add local environment paths and login selection

--- library/src/environments/environment.ts
+++ library/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   demoBankApiBasePath: 'https://bank.demo.cybrid.app',
+  localBankApiBasePath: 'http://api-platform-bank.local.cybrid.com:3002',
   stagingBankApiBasePath: 'https://bank.staging.cybrid.app',
   sandboxBankApiBasePath: 'https://bank.sandbox.cybrid.app',
   productionBankApiBasePath: 'https://bank.production.cybrid.app'
