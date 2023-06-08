9abf6d84be6f754b99dc09b08d7beac4caf088a7
dcocking <dustin.cocking@gmail.com>
2023-06-08 12:22:01 -0400

    remove mocking from account-list e2e

--- cypress/e2e/account-list.cy.ts
+++ cypress/e2e/account-list.cy.ts
@@ -1,5 +1,4 @@
-import { TestConstants } from '@constants';
-
+// @ts-ignore
 function app() {
   return cy.get('app-account-list');
 }
@@ -18,19 +17,6 @@ describe('account-list test', () => {
     cy.authenticate();
     cy.visit('/');
 
-    cy.intercept('GET', 'api/prices*', (req) => {
-      req.reply(TestConstants.SYMBOL_PRICE_BANK_MODEL_ARRAY);
-    }).as('listPrices');
-    cy.intercept('GET', 'api/accounts*', (req) => {
-      req.reply(TestConstants.ACCOUNT_LIST_BANK_MODEL);
-    }).as('listAccounts');
-    cy.intercept('GET', 'api/trades*', (req) => {
-      req.reply(TestConstants.TRADE_LIST_BANK_MODEL);
-    }).as('listTrades');
-    cy.intercept('GET', '/api/customers/*', (req) => {
-      req.reply(TestConstants.CUSTOMER_BANK_MODEL);
-    }).as('getCustomer');
-
     accountListSetup();
   });
 
@@ -39,11 +25,11 @@ describe('account-list test', () => {
   });
 
   it('should display account data', () => {
-    // Check for mocked data and labels
     app()
       .find('#account-value')
-      .should('contain.text', 'Account Value')
-      .should('contain.text', '$12,294,060.30 USD');
+      .should('not.be.empty')
+      .should('contain.text', 'Account Value');
+
     app()
       .find('#assetList')
       .should('contain.text', 'Asset')
@@ -52,18 +38,14 @@ describe('account-list test', () => {
       .should('contain.text', 'USD')
 
       // Check table for ETH account
+      .should('not.be.empty')
       .should('contain.text', 'Ethereum')
       .should('contain.text', 'ETH')
-      .should('contain.text', '4997.7367924308')
-      .should('contain.text', '$1,470.50')
-      .should('contain.text', '$7,349,171.95')
 
       // Check table for BTC account
+      .should('not.be.empty')
       .should('contain.text', 'Bitcoin')
-      .should('contain.text', 'BTC')
-      .should('contain.text', '232.18708499')
-      .should('contain.text', '$21,297.00')
-      .should('contain.text', '$4,944,888.35');
+      .should('contain.text', 'BTC');
   });
 
   it('should navigate back', () => {
@@ -73,7 +55,9 @@ describe('account-list test', () => {
 
   it('should refresh the account list', () => {
     // Intercept listAccounts response
-    let accounts;
+    let accounts: any;
+
+    cy.intercept('GET', 'api/accounts*').as('listAccounts');
     cy.wait('@listAccounts').then((interception) => {
       // @ts-ignore
       accounts = interception.response.body;
@@ -86,26 +70,25 @@ describe('account-list test', () => {
 
   it('should handle errors returned by accounts api', () => {
     // Force accounts error
-    cy.intercept('GET', '/api/accounts*', { forceNetworkError: true }).as(
-      'listAccounts'
-    );
-    cy.wait('@listAccounts');
+    cy.intercept('GET', '/api/accounts*').as('listAccounts');
+
+    cy.wait('@listAccounts').then(() => {
+      cy.intercept('GET', '/api/accounts*', { forceNetworkError: true });
+    });
 
     // Check for error row
     app().find('#warning').should('exist');
-
-    // Reset
-    accountListSetup();
   });
 
   it('should handle errors returned by prices api', () => {
     // Force prices error
-    cy.intercept('GET', '/api/prices*', { forceNetworkError: true }).as(
-      'listPrices'
-    );
+    cy.intercept('GET', '/api/prices*').as('listPrices');
+
     cy.wait('@listPrices').then(() => {
-      // Check for error row
-      app().find('#warning').should('exist');
+      cy.intercept('GET', '/api/prices*', { forceNetworkError: true });
     });
+
+    // Check for error row
+    app().find('#warning').should('exist');
   });
 });
