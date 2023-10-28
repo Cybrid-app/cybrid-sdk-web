64afb5b3b46b0f88761c7e2c7fc7fb83085ce1a8
dcocking <dustin.cocking@gmail.com>
2023-10-28 09:50:54 -0400

    handle storing state

--- library/src/app/components/identity-verification/identity-verification.component.ts
+++ library/src/app/components/identity-verification/identity-verification.component.ts
@@ -181,8 +181,10 @@ export class IdentityVerificationComponent implements OnInit, OnDestroy {
       .pipe(
         map((list) => list.objects[0]),
         switchMap((identity) => {
-          return identity &&
-            identity.state == IdentityVerificationBankModel.StateEnum.Waiting
+          return (identity &&
+            identity.state ==
+              IdentityVerificationBankModel.StateEnum.Waiting) ||
+            identity.state == IdentityVerificationBankModel.StateEnum.Storing
             ? of(identity)
             : this.identityVerificationService.createIdentityVerification();
         }),
